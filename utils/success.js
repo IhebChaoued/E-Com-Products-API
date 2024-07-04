def1c8bc927b84e798b3c160aaeca08f676d1475
Iheb Chaoued <ihebchaoued@gmail.com>
2024-07-04 16:32:37 +0100

    added error/success

---
+++ utils/success.js
@@ -0,0 +1,9 @@
+export const CreateSuccess = (statusCode, successMessage, data) => {
+    const SuccessObj = {
+      status: statusCode,
+      message: successMessage,
+      data: data,
+    };
+    return SuccessObj;
+  };
+  
